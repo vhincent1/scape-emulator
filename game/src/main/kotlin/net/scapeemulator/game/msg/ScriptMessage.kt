b86e172a0459a08197517d92a7ed3d3456853108
vhincent <145734209+vhincent1@users.noreply.github.com>
2025-05-10 23:29:57 -0800

    Fixed RUnClientScript packet
    Signed-off-by: vhincent <145734209+vhincent1@users.noreply.github.com>

--- game/src/main/kotlin/net/scapeemulator/game/msg/ScriptMessage.kt
+++ game/src/main/kotlin/net/scapeemulator/game/msg/ScriptMessage.kt
@@ -1,6 +1,3 @@
 package net.scapeemulator.game.msg
 
-class ScriptMessage(val id: Int, val types: String, vararg parameters: Any) : Message() {
-
-    val parameters: Array<Any> = arrayOf(parameters)
-}
+class ScriptMessage(val id: Int, val types: String, vararg val parameters: Any) : Message()
